c52c2fb79fcf84a9647c0abdd5d2d674877734dc
peytonlundquist <peytonlundquist@u.boisestate.edu>
2022-07-06 14:39:41 -0600

    Dynamic server response & network rework

--- venv/lib/container.py
+++ venv/lib/container.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3           # This is server.py file
+from re import S
 import socket
 import sys
 import threading
 import time
 import atexit
 
+
 class Address:
     def __init__(self, host, port):
         self.host = host
         self.port = port
 
+# Circular list in order to rotate through peer's addresses 
 class NodeList:
     def __init__(self, addr1, addr2):
         self.addr1 = addr1
@@ -23,26 +26,31 @@ class NodeList:
         self.current_node = self.list[self.index]
         return self.current_node
 
-
+# Thread that responds to peer's requests
 class ServerThread(threading.Thread):
     def __init__(self, myport):
         threading.Thread.__init__(self)
         self.myport = myport
 
     def run(self):
-        host = socket.gethostname()
-        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serversocket.bind((host, self.myport))
-        serversocket.listen(5)
-        print("Propping up on " + str(host) + ", " + str(self.myport))
-        while True:
-            clientsocket, addr = serversocket.accept()
-            print("Got a connection from %s" % str(addr))
-            msg = 'Peer' + str(host) + ", " + str(self.myport) + ' pinged you' + "\r"
-            clientsocket.send(msg.encode('ascii'))
-            clientsocket.close()
+        host = socket.gethostname()                                                     # Find my host name, usually localhost
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                # Set up socket
+        serversocket.bind((host, self.myport))                                          # Taking the port specified and attaching this node to it for requests
+        serversocket.listen(5)                                                          # Queue up to 5 requests
+        print("Propping up on " + str(host) + ", " + str(self.myport))                  
+        while True:                                                                     # Enter communication loop
+            clientsocket, addr = serversocket.accept()                                  # Block thread until a client attempts to connect, then accept and open communicatoin line
+            recvmsg = clientsocket.recv(1024)                                           # Expect a message from a client who requests, limiting to 1024 bytes
+            recvmsg = recvmsg.decode('ascii')                                           # The message recieved needs to be decoded
+            print("Server: Recieved " + recvmsg + " from a client connecting to us")
+            if recvmsg == "ping":                                                       # If-else reply block
+                reply = "pong"
+            else:
+                reply = "idk"         
+            clientsocket.send(reply.encode('ascii'))                                    # Encode reply
+            clientsocket.close()                                                        # Close the communication line
 
-
+# Thread that requests for information from peers
 class ClientThread(threading.Thread):
     def __init__(self, peerList):
         threading.Thread.__init__(self)
@@ -50,24 +58,25 @@ class ClientThread(threading.Thread):
         
     def run(self):
         while True:
-            peer = self.peerList.next()
-            time.sleep(1)
+            peer = self.peerList.next()                                                 # Rotate through list of peers
+            time.sleep(1)                                                               # Temporary delay for output analysis
             try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.settimeout(20)
-                s.connect((peer.host, peer.port))
-                msgrecv = s.recv(1024)
-                s.close()
-                print("Recieved:" + msgrecv.decode('ascii'))
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                   # Prepare socket communication
+                s.settimeout(20)                                                        # Socket will timeout after 20 seconds
+                s.connect((peer.host, peer.port))                                       # Connect to peer
+                msg = "ping"    
+                s.send(msg.encode('ascii'))                                             # Make request to peer
+                msgrecv = s.recv(1024)                                                  # Expect a reply
+                s.close()                                                               # Close socket
+                print("Client: Recieved " + msgrecv.decode('ascii') + " from hitting peer at " + str(peer.port))
             except ConnectionRefusedError:
-                print("Peer at " + str(peer.host) + ", " + str(peer.port) + " appears down")
+                print("Client: Peer at " + str(peer.host) + ", " + str(peer.port) + " appears down")
             except ConnectionAbortedError:
-                print("Peer at " + str(peer.host) + ", " + str(peer.port) + " appears down")
+                print("Client: Peer at " + str(peer.host) + ", " + str(peer.port) + " appears down")
 
 
 class Node:
     def __init__(self, myport, port1, port2):
-
         peerList = NodeList(Address(socket.gethostname(), port1), Address(socket.gethostname(), port2))
         thread1 = ServerThread(myport)
         thread1.start()
--- venv/lib/network.py
+++ venv/lib/network.py
@@ -1,5 +1,9 @@
-from container import *
+import random
+from container2 import *
 import multiprocessing
+from numpy.random import default_rng
+random.randint()
 
 for index in range(49153, 49252):
    Node(index-2, index, index+2)
+   
