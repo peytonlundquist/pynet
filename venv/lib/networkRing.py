d4e1e6ba563d6ee1b1c0b5d6aa948089b1d73a88
peytonlundquist <peytonlundquist@u.boisestate.edu>
2022-07-13 16:33:21 -0600

    network ring

---
+++ venv/lib/networkRing.py
@@ -0,0 +1,14 @@
+import random
+from container import *
+import multiprocessing
+import numpy as np
+import random
+
+
+            
+# Create a node
+Node(8000, 0, 8002, True)
+Node(8002, 0, 8004, True)
+Node(8004, 0, 8000, True)
+
+
