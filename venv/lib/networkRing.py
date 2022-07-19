9e3ff0c9068cd0e7a5179f0083ce472fdf1ae1e2
peyton lundquist <peytonlundquist@u.boisestate.edu>
2022-07-19 09:13:50 -0600

    3 peers

--- venv/lib/networkRing.py
+++ venv/lib/networkRing.py
@@ -15,23 +15,27 @@ def dict_hash(dictionary: Dict[str, Any]) -> str:
     return dhash.hexdigest()
 
 node_list = []
+MINIMUM_PORT = 8000
+MAXIMUM_PORT = 8010
 
 def print_chains():
     print("====================================================================")
     for i in range(len(node_list)):      
         node = node_list[i]
-        if(len(node.get_bc()) < 12):
+        if(len(node.get_bc()) < 16):
             print("Node " + str(node.get_port()) + " blockchain: " + str(node.get_bc()))
         else:
             bc_copy = copy.deepcopy(node.get_bc())
             dict(itertools.islice(bc_copy.items(), 12))
-            print("Node " + str(node.get_port()) + " blockchain hash (first 12 blocks): " + dict_hash(dict(itertools.islice(bc_copy.items(), 12))))
+            print("Node " + str(node.get_port()) + " blockchain hash (first 16 blocks): " + dict_hash(dict(itertools.islice(bc_copy.items(), 12))))
 
-for i in range(8000, 8006):
-    print("My port: " + str(i) + ". Peers: " + str(i+2) + ", " + str(i+1) + ", " + str(i-1))
+for i in range(MINIMUM_PORT, MAXIMUM_PORT):
+    print("My port: " + str(i) + ". Peers: " + str(i+2) + ", " + str(i+1))
     node_list.append(Node(i, i+2, i+1, i-1, False))
-node_list.append(Node(8006, 8001, 8000, 8005, False))
-print("My port: " + str(8005) + ". Peers: " + str(8000) + ", " + str(8001) + ", " + str(8004))
+
+# Keystone node - last node in the ring to connect the network
+node_list.append(Node(MAXIMUM_PORT, MINIMUM_PORT + 1, MINIMUM_PORT, MAXIMUM_PORT - 1, False))
+print("My port: " + str(MAXIMUM_PORT) + ". Peers: " + str(MINIMUM_PORT + 1) + ", " + str(MINIMUM_PORT))
 
 while True:
     time.sleep(1)
